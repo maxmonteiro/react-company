1ab4126a7d7509c04ef52d2d720d5fcfd84ef832
Maximo <maxmonteiro86@gmail.com>
2019-03-13 12:34:03 -0300

    correção componente Inicio (homepage)

--- src/Site.js
+++ src/Site.js
@@ -5,35 +5,7 @@ const Site = props => {
     <div>
       
 
-      <div className="container-fluid bg-grey">
-        <div className="row">
-          <div className="col-sm-4">
-            <span className="glyphicon glyphicon-globe logo " />
-          </div>
-          <div className="col-sm-8">
-            <h2>Our Values</h2>
-            <br />
-            <h4>
-              <strong>MISSION:</strong> Our mission lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat.
-            </h4>
-            <br />
-            <p>
-              <strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
-          </div>
-        </div>
-      </div>
+      
 
       {/*<!-- Container (Services Section) -->*/}
       <div id="services" className="container-fluid text-center">
---
+++ src/componentes/Servicos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Servicos = props => {
+    return (
+        
+    )
+}
+
+export default Servicos
