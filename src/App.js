746f93418988f20875e3e13aac23874279000d55
Maximo <maxmonteiro86@gmail.com>
2019-03-13 12:21:46 -0300

    componente Cabeçalho

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import Site from './Site'
 import Rodape from './componentes/Rodape'
+import Cabecalho from './componentes/Cabecalho'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Cabecalho />
         <Site />
         <Rodape />
       </div>
