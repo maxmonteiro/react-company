e99f4b647c122ef5639fe24d99e3ee2ffab7178a
Maximo <maxmonteiro86@gmail.com>
2019-03-13 12:30:03 -0300

    componente Inicio (homepage)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import Site from './Site'
 import Rodape from './componentes/Rodape'
 import Cabecalho from './componentes/Cabecalho'
+import Inicio from './componentes/Inicio'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Cabecalho />
+        <Inicio />
         <Site />
         <Rodape />
       </div>
