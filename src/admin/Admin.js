f235a1f86283b1ab5b8443c3a2d0d963395ef733
Maximo <maxmonteiro86@gmail.com>
2019-03-16 14:47:27 -0300

    criado componente AdminMenu e navegação

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,15 +1,20 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import { Route } from 'react-router-dom'
+import AdminMenu from "./AdminMenu";
 
 class Admin extends Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <h2>Painel Administrativo</h2>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h2>Painel Administrativo</h2>
+        <Route path={'/'} component={AdminMenu}></Route>
+      </div>
+    );
+  }
 }
 
-export default Admin
+export default Admin;
---
+++ src/admin/AdminMenu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AdminMenu = props => {
+  return (
+    <div className="list-group">
+      <a href="#" className="list-group-item list-group-item-action active">
+        Selecione uma opção
+      </a>
+      <a href="#" className="list-group-item list-group-item-action">
+        Portfólio
+      </a>
+    </div>
+  );
+};
+
+export default AdminMenu;
