2af788f7cfa09847cb9350e3b57b9c842a972b35
Maximo <maxmonteiro86@gmail.com>
2019-03-16 14:59:31 -0300

    criado componente AdminPortfolio e navegação

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom';
+
 import AdminMenu from "./AdminMenu";
+import AdminPortfolio from "./AdminPortfolio";
 
 class Admin extends Component {
   constructor(props) {
@@ -12,6 +14,9 @@ class Admin extends Component {
       <div>
         <h2>Painel Administrativo</h2>
         <Route path={'/'} component={AdminMenu}></Route>
+        {/* Exibe o conteudo do props, onde está o obj match usado para construir a rota */}
+        {/* <p>{JSON.stringify(this.props)</p> */}
+        <Route path={`${this.props.match.url}/portfolio`} component={AdminPortfolio}></Route>
       </div>
     );
   }
