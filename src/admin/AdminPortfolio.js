083e447bcad2f8ffdd5e5fcabc5fe42e019df190
Maximo <maxmonteiro86@gmail.com>
2019-03-27 14:11:59 -0300

    Criando formulário de Portfólio

--- src/admin/AdminPortfolio.js
+++ src/admin/AdminPortfolio.js
@@ -8,7 +8,24 @@ class AdminPortfolio extends Component {
 
     render() {
         return (
-            <h2>Potrfólio - Área Administrativa</h2>
+            <div style={{padding: '100px'}}>
+                <h2>Potrfólio - Área Administrativa</h2>
+                <form>
+                    <div className="form-group">
+                        <label for="titulo">Título</label>
+                        <input type="text" className="form-control" id="titulo" placeholder="Título" />
+                    </div>
+                    <div className="form-group">
+                        <label for="descricao">Descrição</label>
+                        <textarea className="form-control" id="descricao"></textarea>
+                    </div>
+                    <div className="form-group">
+                        <label for="imagem">Imagem</label>
+                        <input type="file" className="form-control" id="imagem" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Salvar</button>
+                </form>
+            </div>
         )
     }
 }
