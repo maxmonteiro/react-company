8952e4c65dab7e27d250dbe109e3413a368e8e94
Maximo <maxmonteiro86@gmail.com>
2019-04-03 14:39:04 -0300

    Conectanco o form de Login

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -3,24 +3,33 @@ import React, { Component } from "react";
 class Login extends Component {
   constructor(props) {
     super(props);
+
+    this.email = null;
+    this.senha = null;
+
+    this.autenticaUsuario = this.autenticaUsuario.bind(this);
+  }
+
+  autenticaUsuario() {
+    console.log(this.email.value, this.senha.value);
   }
 
   render() {
     return (
       <div className="container">
         <h1 className="text-center">Login</h1>
-        <form className="col-md-4 col-md-offset-4">
+        <div className="col-md-4 col-md-offset-4">
           <div className="form-group">
             <label htmlFor="exampleInputEmail1">Email</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <input type="email" name="email" ref={ref => this.email = ref} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div className="form-group">
             <label htmlFor="exampleInputPassword1">Senha</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
+            <input type="password" name="senha" ref={ref => this.senha = ref} className="form-control" id="exampleInputPassword1" />
           </div>
-          <button type="submit" className="btn btn-primary">Entrar</button>
-        </form>
+          <button type="button" className="btn btn-primary" onClick={this.autenticaUsuario}>Entrar</button>
+        </div>
       </div>
     );
   }
