06ff9d94477a663ad8e4f065777e64ae8407fff8
Maximo <maxmonteiro86@gmail.com>
2019-04-03 14:37:44 -0300

    Ajustando estilo componente formulário de Login

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -11,12 +11,12 @@ class Login extends Component {
         <h1 className="text-center">Login</h1>
         <form className="col-md-4 col-md-offset-4">
           <div className="form-group">
-            <label forHTML="exampleInputEmail1">Email</label>
+            <label htmlFor="exampleInputEmail1">Email</label>
             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div className="form-group">
-            <label forHTML="exampleInputPassword1">Senha</label>
+            <label htmlFor="exampleInputPassword1">Senha</label>
             <input type="password" className="form-control" id="exampleInputPassword1" />
           </div>
           <button type="submit" className="btn btn-primary">Entrar</button>
