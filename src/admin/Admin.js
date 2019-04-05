322c0a8de8d906d4fede939de95ea622f7797b04
Maximo <maxmonteiro86@gmail.com>
2019-04-05 11:52:05 -0300

    Protegendo a rota Admin

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Route } from 'react-router-dom';
+import { auth } from './../firebase-config';
+import { Redirect } from 'react-router-dom'
 
 import AdminMenu from "./AdminMenu";
 import AdminPortfolio from "./AdminPortfolio";
@@ -7,15 +9,37 @@ import AdminPortfolio from "./AdminPortfolio";
 class Admin extends Component {
   constructor(props) {
     super(props);
+    // estados da autenticação
+    this.state = {
+      autenticado: false,
+      logando: true,
+      user: null
+    }
+  }
+
+  // verificar assim que o estado de autenticação mudar
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({
+        logando: false,
+        autenticado: !!user,
+        user // parametro igual ao estado, não é necessário user:user
+      })
+    })
   }
 
   render() {
+    if (this.state.logando) {
+      return <p>Aguarde...</p>
+    }
+    // não está logado
+    if (!this.state.autenticado) {
+      return <Redirect to="/login"></Redirect>
+    }
     return (
       <div>
         <h2>Painel Administrativo</h2>
         <Route path={'/'} component={AdminMenu}></Route>
-        {/* Exibe o conteudo do props, onde está o obj match usado para construir a rota */}
-        {/* <p>{JSON.stringify(this.props)</p> */}
         <Route path={`${this.props.match.url}/portfolio`} component={AdminPortfolio}></Route>
       </div>
     );
